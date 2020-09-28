3a43778af82f6108f71c7a496c0d7a6b866ffc7b
eakubilo <eakubilo@wesleyan.edu>
2020-09-27 21:50:49 -0400

    Added user input functionality to graph

--- mathplot.py
+++ mathplot.py
@@ -2,21 +2,39 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
 NavigationToolbar2Tk)
 import math
+import numpy
 
-def plot(window):
+
+def sin(x):
+    return numpy.sin(x)
+def cos(x):
+    return numpy.cos(x)
+def tan(x):
+    return numpy.tan(x)
+def sqrt(x):
+    return numpy.sqrt(x)
+def plot(window, func):
+    counter = 0
+    k = len(window.winfo_children())
+    while counter < k:
+        if counter == 2:
+            window.winfo_children()[2].destroy()
+            k-=1
+        else:
+            counter+=1
+        
     fig = Figure(figsize=(5,5), dpi = 100)
 
-    y = [math.sin(1/(i/1000)) for i in range(-1000,1000) if i != 0]
-    x = [i/1000 for i in range(-1000,1000) if i != 0]
-    print(y,x)
+    x = numpy.arange(-100.,100.,.0001)
+
     plot1 = fig.add_subplot(111)
-
-    plot1.plot(x,y)
-
+    plot1.set_ylim(-1,1)
+    plot1.set_xlim(-1,1)
+    plot1.plot(x,eval(func))
+    plot1.grid(b=True, which = "both", axis = "both")
     canvas = FigureCanvasTkAgg(fig,master = window)
-
+    
     canvas.draw()
-
     canvas.get_tk_widget().pack()
 
     toolbar = NavigationToolbar2Tk(canvas,window)
--- tkinterwindow.py
+++ tkinterwindow.py
@@ -2,13 +2,16 @@ from tkinter import *
 from mathplot import plot
 
 window = Tk()
-
+canvas = 0
+fig = 0
 window.title('Plotting in Tkinter')
 
 window.geometry =("500x500")
 
-plot_button = Button(master = window, command = lambda: plot(window), height = 2, width = 10, text = "Plot")
+e = Entry(window)
+
+plot_button = Button(master = window, command = lambda: plot(window,e.get()), height = 2, width = 10, text = "Plot")
 
 plot_button.pack()
-
+e.pack()
 window.mainloop()
